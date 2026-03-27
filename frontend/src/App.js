7dfacf2ee05a364330618167a2ef14ff9da374d5
Simmonds <jake.simmonds@capgemini.com>
2026-03-27 12:04:15 +0000

    Add basic movie card UI with image and buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+const movie = {
+  title: "Inception",
+  image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px"}}>
+      <div style={{
+        border: "1px solid #ccc",
+        borderRadius: "10px", 
+        padding: "20px",
+        width: "320px",
+        textAlign: "center",
+        boxShadow: "0 4px 10px rgba(0,0,0,0.1)"
+      }}>
+        <h2>{movie.title}</h2>
+
+        <img
+          src={movie.image}
+          alt={movie.title}
+          style={{ width: "100%", borderRadius: "10ox"}}
+        />
+
+        <div style={{ marginTop: "15px" }}>
+          <button style={{ marginRight: "10px" }}>Dislike 👎</button>
+          <button>Like 👍</button>
+        </div>
+      </div>
     </div>
   );
 }
